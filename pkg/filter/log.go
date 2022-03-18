3ef7434f3355ded7a7ef7b65a4a445d86fd4afcc
st0n3 <ssst0n3@gmail.com>
2022-03-18 21:31:55 +0800

    add filter/LogSwitch

--- pkg/filter/log.go
+++ pkg/filter/log.go
@@ -4,11 +4,19 @@ import (
 	"github.com/ssst0n3/awesome_libs/log"
 )
 
+var LogSwitch = true
+
 func check(f Filter, target Object) {
+	if !LogSwitch {
+		return
+	}
 	log.Logger.Debugf("%s start checking %s", f, target)
 }
 
 func debug(f Filter, rule string, target Object, allow bool) {
+	if !LogSwitch {
+		return
+	}
 	action := "deny"
 	if allow {
 		action = "allow"
